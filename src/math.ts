faaed0a9e9dc18ee3d0e0c1b54fdc2871d05ff32
gregs <ukfaeku@gmail.com>
2024-09-05 17:30:47 -0300

    lerpRadians

--- src/math.ts
+++ src/math.ts
@@ -1,30 +1,10 @@
 import { Vector } from 'three/examples/jsm/Addons.js'
-import { lerp } from 'three/src/math/MathUtils.js'
 
 type Radians = number // number between Pi and -Pi
 
-const PI_TIMES_TWO = Math.PI * 2
-export function lerpRadians(angleFrom: Radians, angleTo: Radians, lerpFactor: number) {
-  const diff = angleTo - angleFrom
-
-  if (diff < -Math.PI) {
-    // lerp upwards past PI_TIMES_TWO
-    angleTo += PI_TIMES_TWO
-    const result = lerp(angleFrom, angleTo, lerpFactor)
-    if (result >= PI_TIMES_TWO) return result - PI_TIMES_TWO
-    return result
-  }
-
-  if (diff > Math.PI) {
-    // lerp downwards past 0
-    angleTo -= PI_TIMES_TWO
-    const result = lerp(angleFrom, angleTo, lerpFactor)
-    if (result < 0) return result + PI_TIMES_TWO
-    return result
-  }
-
-  // straight lerp
-  return lerp(angleFrom, angleTo, lerpFactor)
+export function lerpRadians(angleFrom: Radians, angleTo: Radians, lerpFactor: number): Radians {
+  const diff = ((angleTo - angleFrom + Math.PI * 3) % (Math.PI * 2)) - Math.PI
+  return (angleFrom + diff * lerpFactor + Math.PI * 2) % (Math.PI * 2)
 }
 
 export function add(a: Vector, b: Vector): Vector {
