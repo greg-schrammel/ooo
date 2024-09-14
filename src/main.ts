a2c4de887d78ca662e604a0b7768c26a18e94247
gregs <ukfaeku@gmail.com>
2024-09-14 04:20:57 -0300

    aaa

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,56 @@
 import {
-  Scene,
-  PerspectiveCamera,
-  WebGLRenderer,
-  BoxGeometry,
-  MeshBasicMaterial,
-  Mesh,
-  Vector3,
-  GridHelper,
-  EdgesGeometry,
-  LineSegments,
-  LineBasicMaterial,
-  Color,
-  Fog,
-  CustomToneMapping,
-  BufferGeometry,
-  Line,
-  Group,
-  Clock,
-  BufferAttribute,
   AmbientLight,
-  HemisphereLight,
-  AnimationMixer,
-  AnimationClip,
   AnimationAction,
+  AnimationClip,
+  AnimationMixer,
+  BoxGeometry,
+  BufferAttribute,
+  BufferGeometry,
+  Clock,
+  Color,
+  EdgesGeometry,
+  Fog,
+  GridHelper,
+  Group,
+  HemisphereLight,
+  Line,
+  LineBasicMaterial,
+  LineSegments,
+  Mesh,
+  MeshBasicMaterial,
+  NoToneMapping,
+  Object3D,
+  PerspectiveCamera,
+  Scene,
+  Vector3,
+  WebGLRenderer,
 } from 'three'
-import { $activeControls, addControlsListeners } from './controls'
 import { clamp } from 'three/src/math/MathUtils.js'
-import { add, lerpRadians } from './math'
+import { $activeControls, addControlsListeners } from './controls'
+import { lerpRadians } from './math'
 
-import { World, ColliderDesc, RigidBodyDesc, init, Collider } from '@dimforge/rapier3d-compat'
+import {
+  ActiveCollisionTypes,
+  Collider,
+  ColliderDesc,
+  EventQueue,
+  init,
+  QueryFilterFlags,
+  RigidBodyDesc,
+  World,
+} from '@dimforge/rapier3d-compat'
 import Stats from 'three/examples/jsm/libs/stats.module.js'
 
+import {
+  BlendFunction,
+  EffectComposer,
+  EffectPass,
+  OutlineEffect,
+  RenderPass,
+  SMAAEffect,
+  ToneMappingEffect,
+  ToneMappingMode,
+} from 'postprocessing'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 await init()
@@ -47,8 +67,13 @@ function perspectiveCamera() {
 }
 
 function setupRenderer() {
-  const renderer = new WebGLRenderer({ antialias: true })
-  renderer.toneMapping = CustomToneMapping
+  const renderer = new WebGLRenderer({
+    powerPreference: 'high-performance',
+    antialias: false,
+    stencil: false,
+    depth: false,
+  })
+  renderer.toneMapping = NoToneMapping
   renderer.setSize(window.innerWidth, window.innerHeight)
   renderer.setPixelRatio(window.devicePixelRatio * 1.5)
   return renderer
@@ -80,16 +105,7 @@ function createPlayerObject3d(position: Vector3) {
   const group = new Group()
   group.position.add(position)
 
-  const facingDirectionLine = new Line(
-    new BufferGeometry().setFromPoints([new Vector3(0, 0, 0), new Vector3(0, 0, 1.2)]),
-    new LineBasicMaterial({ color: 'black' }),
-  )
-
   group.add(barbarian.scene)
-
-  if (_debug_) {
-    group.add(facingDirectionLine)
-  }
 
   return group
 }
@@ -103,17 +119,17 @@ function createCharacterObject3d(position: Vector3) {
   const cube = new Mesh(geometry, material)
   cube.position.y = cube.geometry.parameters.height / 2
 
-  const edges = new EdgesGeometry(geometry)
-  const edgesLines = new LineSegments(edges, new LineBasicMaterial({ color: 0xfff }))
-
-  const facingDirectionLine = new Line(
-    new BufferGeometry().setFromPoints([new Vector3(0, 0, 0), new Vector3(0, 0, 1.2)]),
-    new LineBasicMaterial({ color: 'black' }),
-  )
-
   group.add(cube)
 
   if (_debug_) {
+    const edges = new EdgesGeometry(geometry)
+    const edgesLines = new LineSegments(edges, new LineBasicMaterial({ color: 0xfff }))
+
+    const facingDirectionLine = new Line(
+      new BufferGeometry().setFromPoints([new Vector3(0, 0, 0), new Vector3(0, 0, 1.2)]),
+      new LineBasicMaterial({ color: 'black' }),
+    )
+
     cube.add(edgesLines)
     group.add(facingDirectionLine)
   }
@@ -136,25 +152,66 @@ function createCharacterRigidBody(position: Vector3) {
 }
 
 const position = new Vector3(0, 0, 0)
+const { rigidBody, bodyCollider } = createCharacterRigidBody(position)
 const $player = {
   position,
   rotation: 0,
   lastMoveAt: 0,
   speed: 1,
   object3D: createPlayerObject3d(position),
-  ...createCharacterRigidBody(position),
+  rigidBody,
+  bodyCollider,
 }
 
 const $scene = setupScene()
 const $camera = perspectiveCamera()
 const $renderer = setupRenderer()
+
+const outlineEffect = new OutlineEffect($scene, $camera, {
+  multisampling: Math.min(4, $renderer.capabilities.maxSamples),
+  edgeStrength: 10,
+  blendFunction: BlendFunction.ALPHA,
+  visibleEdgeColor: 0xffffff,
+})
+outlineEffect.blurPass.enabled = true
+
+function postprocessing() {
+  const composer = new EffectComposer($renderer)
+  composer.addPass(new RenderPass($scene, $camera))
+
+  // const shockWaveEffect = new ShockWaveEffect($camera, new Vector3(3, 0, 3), {
+  //   speed: 0.5,
+  //   maxRadius: 0.1,
+  //   waveSize: 0.1,
+  //   amplitude: 0.01,
+  // })
+  // const effectPass = new EffectPass($camera, shockWaveEffect)
+  // composer.addPass(effectPass)
+  // setInterval(() => shockWaveEffect.explode(), 1000)
+
+  composer.addPass(new EffectPass($camera, outlineEffect))
+
+  composer.addPass(new EffectPass($camera, new SMAAEffect()))
+  composer.addPass(new EffectPass($camera, new ToneMappingEffect({ mode: ToneMappingMode.AGX })))
+
+  // composer.addPass(new EffectPass($camera, new NoiseEffect({ premultiply: true })))
+
+  return composer
+}
+
+const $composer = postprocessing()
+
 document.body.appendChild($renderer.domElement)
 
 $scene.add($player.object3D)
 
 // npc
 const npc_position = new Vector3(3, 0, 3)
-createCharacterRigidBody(npc_position)
+
+const box_rigidBody = $world.createRigidBody(RigidBodyDesc.dynamic())
+box_rigidBody.setTranslation({ x: 3, y: 0, z: 3 }, true)
+const bodyColliderDesc = ColliderDesc.cylinder(0.5, 0.7).setTranslation(0, 0.5, 0)
+const boxCollider = $world.createCollider(bodyColliderDesc, box_rigidBody)
 $scene.add(createCharacterObject3d(npc_position))
 //
 
@@ -209,10 +266,15 @@ const runClip = AnimationClip.findByName(barbarian.animations, 'Running_A')
 const runAction = mixer.clipAction(runClip)
 runAction.timeScale = 1.5
 
+const attackClip = AnimationClip.findByName(barbarian.animations, 'Dualwield_Melee_Attack_Chop')
+const attackAction = mixer.clipAction(attackClip)
+
+// console.log(barbarian.animations.map((clip) => clip.name))
+
 let currentAction: AnimationAction = idleAction.play()
 
 const updatePlayerPosition = (deltaTime: number) => {
-  const isMoving = $player.object3D.position.distanceTo($player.position) >= 0.1
+  const isMoving = $player.object3D.position.distanceTo($player.position) >= 0.08
   const targetAction = isMoving ? runAction : idleAction
 
   if (currentAction !== targetAction) {
@@ -244,25 +306,65 @@ function updateRapierDebugMesh() {
 
 function computeMoviment(collider: Collider, impulse: Vector3) {
   const controller = $world.createCharacterController(0.05)
-  controller.computeColliderMovement(collider, impulse)
+  controller.computeColliderMovement(collider, impulse, QueryFilterFlags.EXCLUDE_SENSORS)
   const correctedMovement = controller.computedMovement()
   return correctedMovement
 }
 
-function onTick() {
-  $world.step()
+const eventQueue = new EventQueue(true)
 
+const pillar = await loader.loadAsync('pillar.glb')
+pillar.scene.position.set(-3, 0, -3)
+pillar.scene.rotateY(Math.PI / 2)
+$scene.add(pillar.scene)
+function createArea() {
+  const areaSensor = ColliderDesc.cylinder(1, 3)
+  areaSensor.setSensor(true)
+  areaSensor.translation = pillar.scene.position
+  areaSensor.setActiveCollisionTypes(ActiveCollisionTypes.KINEMATIC_FIXED)
+  const sensor = $world.createCollider(areaSensor)
+
+  const pillar_rigidBody = $world.createRigidBody(RigidBodyDesc.fixed())
+  pillar_rigidBody.setTranslation(pillar.scene.position, false)
+  const pillarColliderDesc = ColliderDesc.cylinder(0.5, 0.7)
+  $world.createCollider(pillarColliderDesc, pillar_rigidBody)
+
+  return sensor
+}
+
+const areaCollider = createArea()
+
+const white = new Color(0xffffff)
+const red = new Color(0xff0000)
+const blue = new Color(0x0055ff)
+
+function outline(object3D: Object3D) {
+  object3D.traverse((o) => outlineEffect.selection.add(o))
+}
+
+function onTick() {
   const impulse = impulseInput()
   const movement = computeMoviment($player.bodyCollider, impulse)
-  const position = add($player.position, movement)
-  $player.position = new Vector3(position.x, position.y, position.z)
-  $player.rigidBody.setNextKinematicTranslation(position)
+  $player.position.add(movement)
+  $player.rigidBody.setNextKinematicTranslation($player.position)
+  if (impulse.length()) $player.rotation = Math.atan2(impulse.x, impulse.z)
 
   if (_debug_) {
     updateRapierDebugMesh()
   }
 
-  if (impulse.length()) $player.rotation = Math.atan2(impulse.x, impulse.z)
+  $world.step(eventQueue)
+
+  outline($player.object3D)
+
+  outlineEffect.visibleEdgeColor = white
+  outlineEffect.pulseSpeed = 0
+
+  const isInsideArea = $world.intersectionPair($player.bodyCollider, areaCollider)
+  if (isInsideArea) {
+    outlineEffect.visibleEdgeColor = blue
+    outlineEffect.pulseSpeed = 0.2
+  }
 }
 
 const clock = new Clock()
@@ -279,6 +381,8 @@ function animate() {
   mixer.update(deltaTime)
 
   stats.update()
+
+  $composer.render(deltaTime)
 }
 $renderer.setAnimationLoop(animate)
 setInterval(onTick, TICK_ms)
